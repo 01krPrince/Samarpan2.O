ec93d11a9bbc5fbfc4de488e4fc0371b211e84d7
Prince kumar <princekr3006@gmail.com>
2025-03-20 19:51:42 +0530

    Branch and User completed

---
+++ src/main/java/com/example/Samarpan2/O/model/Branch.java
@@ -0,0 +1,22 @@
+package com.example.Samarpan2.O.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "branch")
+public class Branch {
+    @Id
+    private String id;
+
+    private String branchName;
+}
---
+++ src/main/java/com/example/Samarpan2/O/service/Impl/BranchServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.Samarpan2.O.service.Impl;
+
+import com.example.Samarpan2.O.model.Branch;
+import com.example.Samarpan2.O.repository.BranchRepository;
+import com.example.Samarpan2.O.service.BranchService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BranchServiceImpl implements BranchService {
+
+    private final BranchRepository branchRepository;
+
+    public BranchServiceImpl (BranchRepository branchRepository) {
+        this.branchRepository = branchRepository;
+    }
+
+    @Override
+    public Branch createBranch(String branchName) {
+        Branch branch = branchRepository.findByBranchName(branchName.trim().toUpperCase());
+        return null;
+    }
+
+    @Override
+    public List<Branch> getAllBranches() {
+        return branchRepository.findAll();
+    }
+
+    @Override
+    public Optional<Branch> findById(String branchId) {
+        return branchRepository.findById(branchId);
+    }
+}
