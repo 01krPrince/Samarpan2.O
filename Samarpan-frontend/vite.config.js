289d61c8834616f9ad9e24bdc352fb753724a2f2
Prince kumar <princekr3006@gmail.com>
2025-04-11 14:14:47 +0530

    builded

--- Samarpan-frontend/vite.config.js
+++ Samarpan-frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,  tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    copy({
+      targets: [{ src: 'public/_redirects', dest: 'dist' }],
+      hook: 'writeBundle',
+    }),
+  ],
 })
-
-
